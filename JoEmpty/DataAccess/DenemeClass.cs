4e61341fe3775deb573d99746f77a9e4ab77ac46
Enes Celik <enes.celik.41@hotmail.com>
2019-03-07 03:03:38 +0300

    Database/DenemeClass Eklendi.

---
+++ JoEmpty/DataAccess/DenemeClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class DenemeClass
+    {
+        public int Deneme { get; set; }
+    }
+}
