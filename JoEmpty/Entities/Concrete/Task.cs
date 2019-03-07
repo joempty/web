91480c34e21891344d88b3a3bb9bf9b388126cd0
Ersoy TORAMAN <jovannijares@gmail.com>
2019-03-08 01:38:14 +0300

    Task varlığı eklendi.

---
+++ JoEmpty/Entities/Concrete/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
