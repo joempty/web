7d27227f0317c7a451e3541ba091020c6694ee92
Ersoy TORAMAN <jovannijares@gmail.com>
2019-03-08 01:37:41 +0300

    Business katmanının temelleri atıldı.

---
+++ JoEmpty/Business/Concrete/TaskManager.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class TaskManager : ITaskService
+    {
+        public void Add(Task task)
+        {
+            //add to database
+        }
+
+        public List<Task> ListAll()
+        {
+            //retun list from database
+            return null;
+        }
+    }
+}
