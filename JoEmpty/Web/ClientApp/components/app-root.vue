4f02b0136de98cde51fc4e6e374b136f5718e5e4
Yakup Melih Çakır <yakupcakr@hotmail.com>
2019-03-12 00:35:52 +0300

    Starter template eklendi ve tema uyarlandı.

---
+++ JoEmpty/Web/ClientApp/components/app-root.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-1">
+        <!-- Layout inner -->
+        <div class="layout-inner">
+            <nav-bar></nav-bar>
+            <div class="layout-container" id="app">
+            <nav-menu params="route: route"></nav-menu>
+                <!-- Page content -->
+                <div class="layout-content">
+                    <div class="container-fluid flex-grow-1 container-p-y">
+                        <router-view></router-view>
+                    </div>
+                </div>
+                <!-- / Page content -->
+            </div>
+        </div>
+        <!-- Layout inner -->
+    </div>
+    <!-- / Layout wrapper -->
+</template>
+
+<script>
+    import NavMenu from './nav-menu'
+    import NavBar from './nav-bar'
+
+    export default {
+      components: {
+        'nav-menu': NavMenu,
+        'nav-bar' : NavBar
+      },
+
+      data () {
+        return {}
+      }
+    }
+</script>
+
+<style>
+</style>
---
+++ JoEmpty/Web/ClientApp/components/nav-bar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- Layout navbar -->
+        <nav class="layout-navbar navbar navbar-expand-lg align-items-lg-center bg-navbar-theme container-p-x" id="layout-navbar">
+            <a href="index.html" class="navbar-brand">JOVASOFT</a>
+
+            <!-- Sidenav toggle -->
+            <div class="layout-sidenav-toggle navbar-nav align-items-lg-center mr-auto mr-lg-4">
+                <a class="nav-item nav-link px-0 ml-2" href="javascript:void(0)">
+                <i class="ion ion-md-menu text-large align-middle"></i>
+                </a>
+            </div>
+
+            <!-- Navbar toggle -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#layout-navbar-collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+        </nav>
+        <!-- / Layout navbar -->
+    </div>
+</template>
