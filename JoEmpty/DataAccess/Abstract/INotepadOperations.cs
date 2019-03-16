9ed65000cd8773da0199ef1e7a8e157b52d8c548
eecelik <enescelik.41@gmail.com>
2019-03-16 21:54:43 +0300

    Database Operasyonları Yazıldı.

---
+++ JoEmpty/DataAccess/Abstract/INotepadOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface INotepadOperations
+    {
+        string[] Read(string path);
+        void Write(string path, string text);
+        void Delete(string path, string text);
+    }
+}
